fe280d180e9f890e0d94b93f6f9e8e3b95508cf9
krimohshu <krishan.shyam@gmail.com>
2017-10-23 12:01:46 +0100

    Updated comment to have alternate ways of getting properties.

--- src/main/java/com/yogi/common/Base.java
+++ src/main/java/com/yogi/common/Base.java
@@ -7,6 +7,12 @@ import java.util.Properties;
 public class Base {
     String path="application.properties";
 
+    /*== Just for demo purpose of traditional property access otherwise this is not required
+         1. Use Spring @Value to get the property from Yml etc
+         2. Alternatively Autowired Environment to get property e.g. from app.{xxx}.properties
+         3. Use Java8 Files and Path and stream api to manipulate site easily
+     */
+    
     public String getProperty(String itemType){
 
         Properties prop = new Properties();
